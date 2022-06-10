7c8822ca4295481bd5f130ee7d163317fd833bbd
vivian568 <65995169+vivian568@users.noreply.github.com>
2022-06-09 22:05:35 -0400

    commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-  
+import "./AddTodo.js";
+import "./todos.js";
+import "./Home.css";
+
 class Home extends Component {
   // A default state of this component with an empty list of todos.
-  constructor() {
+  constructor(todos) {
     super();
     this.state = {
-      // create your empty list here call it todos.
+      todos: []
     };
   }
   // the addTodo function simply creates a new array that includes the user submitted todo item and then
@@ -28,7 +31,10 @@ class Home extends Component {
     return (
       <div className="Home">
         <h1>Todo's </h1>
-        <p> Replace this</p>
+
+        <Todos todos={this.state.todos} />
+        <hr>/</hr>
+        <AddToDo addTodo={this.addTodo} />
       </div>
     );
   }
